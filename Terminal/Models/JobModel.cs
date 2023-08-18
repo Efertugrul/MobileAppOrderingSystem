8ef58ddf79a6505a73bd66a584f48c4a2a34dd7c
Efe Ertugrul <107368002+Efertugrul@users.noreply.github.com>
2023-08-18 20:35:27 +0300

    Add files via upload

---
+++ Terminal/Models/JobModel.cs
@@ -0,0 +1,18 @@
+﻿namespace RIVA.Models
+{
+    public class JobModel
+    {
+        public int Id { get; set; }
+        public JobStatuses Status { get; set; }
+        public string Description { get; set; }
+        public DateTime LastUpdated { get; set; }
+        public string idmumber { get; set; }
+    }
+
+    public enum JobStatuses
+    {
+        Todo,
+        Started,
+        Completed
+    }
+}
